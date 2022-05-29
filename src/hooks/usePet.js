e71dab05a18b7c7f2fd91fd0af8906fb16c318f3
Ben Levi <benschool01@gmail.com>
2022-05-29 19:26:13 +0300

    yossi changes

--- src/hooks/usePet.js
+++ src/hooks/usePet.js
@@ -4,19 +4,6 @@ import { useMutation, useQuery, useQueryClient } from 'react-query';
 
 const usePet = (petId) => {
   // const queryClient = useQueryClient();
-  const petQuery = useQuery(['pet', petId], async () => {
-    const { data } = await axios.get(`${CONFIG_URLS.base}/pets/${petId}`);
-    return data;
-  }, {
-    refetchOnMount: false,
-    refetchOnWindowFocus: false,
-  });
-
-  const updateStatus = useMutation((d) => axios.post(`${CONFIG_URLS.base}/pets/${petId}/status`, d), {
-    onSuccess: () => {
-      console.log('ON SUCCESS');
-    }
-  });
 
   return {
     petQuery,
