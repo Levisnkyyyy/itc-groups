84f25e85cbdd42f9b3fd5c2ab3cb5c0e002e25a2
Ben Levi <benschool01@gmail.com>
2022-05-29 19:21:55 +0300

    changes whole file

--- src/hooks/usePet.js
+++ src/hooks/usePet.js
@@ -1,25 +1,42 @@
-import CONFIG_URLS from '../config/urls';
+import { useMutation, useQuery } from 'react-query';
+import CONFIG_URLS from "../config/urls";
 import axios from "../config/axios";
-import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { useState } from 'react';
 
-const usePet = (petId) => {
-  // const queryClient = useQueryClient();
-  const petQuery = useMutation((d) => axios.post(`${CONFIG_URLS.base}/pets/${petId}/status`, d), {
+export default function usePets() {
+  const [filter, setFilter] = useState([]);
+  const { data, isLoading, refetch: refetchPets } = useQuery(['pets', filter.map((f) => ({ ...f, id: undefined }))], async () => {
+    if (filter.length > 0) {
+      const response = await axios.post(`${CONFIG_URLS.base}/pets/search`, filter);
+      return response.data;
+    }
+    const response = await axios.get(`${CONFIG_URLS.base}/pets`);
+    return response.data;
+  }, {
+    placeholderData: [],
+    cacheTime: 60 * 60 * 60,
+    staleTime: Infinity,
+    refetchOnMount: true,
+  });
+
+  const addNewPet = useMutation((d) => axios.post(`${CONFIG_URLS.base}/pets`, d), {
     onSuccess: () => {
-      console.log('ON SUCCESS');
+      refetchPets();
     }
   });
 
-  const updateStatus = useMutation((d) => axios.post(`${CONFIG_URLS.base}/pets/${petId}/status`, d), {
+  const deletePet = useMutation((petId) => axios.delete(`${CONFIG_URLS.base}/pets/${petId}`), {
     onSuccess: () => {
-      console.log('ON SUCCESS');
+      refetchPets();
     }
-  });
+  })
 
   return {
-    petQuery,
-    updatePetStatus,
+    filter,
+    setFilter,
+    data,
+    isLoading,
+    addNewPet,
+    deletePet,
   }
 }
-
-export default usePet;
